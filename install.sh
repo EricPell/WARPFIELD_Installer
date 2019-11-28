80c1d61169309ac9f7b4c04995df91b0c5f2855d
Eric Pellegrini <ericpellegrini@uni-heidelberg.de>
2019-11-28 15:43:40 +0100

    simplified installer for local access of git

--- install.sh
+++ install.sh
@@ -1,23 +1,31 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 VERSION="2_2"
-if [[ $PWD == *"WARPFIELD_Installer"* ]]; then
-    echo -e "${RED}!!!! WARNING !!!!${NC}"
-    echo -e "${RED} WARNING: You are in the WARPFIELD_Installer directory${NC}"
-    echo -e "Do not install WARPFIELD packages here."
-    echo -e "Go to the directory where you will install warpfield packes, and rerun this script"
-else
-    git clone https://github.com/EricPell/warpfield_core
-    git clone https://github.com/EricPell/warpfield_emp
-    git clone https://github.com/EricPell/warpfield_emp_viewer
-    pip3 install -I pyqt5, astropy
 
-    echo "export WARPFIELD_CORE=$PWD/warpfield_core/" > ~/.warpfield_init.sh
-    echo "export WARPFIELD_EMP_DEBUG=False" >> ~/.warpfield_init.sh
-    echo "export WARPFIELD_EMP_DEFAULT_TABLE=$WARPFIELD_CORE/public_data/cloud_data/warpfield_emp_1d_v1.0.ecsv" >> ~/.warpfield_init.sh
-    echo "export PYTHONPATH=\$PYTHONPATH:$PWD" >> ~/.warpfield_init.sh
-    echo "alias warpfield_gui=\"python3 $PWD/warpfield_emp_viewer/WARPEMP_VIEWER_v${VERSION}.py\"" >> ~/.warpfield_init.sh
+pip3 install -I pyqt5 astropy
 
-    echo -e "\n#The following line was added by the WARPFIELD Installer script" >> ~/.bashrc
-    echo -e "source ~/.warpfield_init.sh\n" >> ~/.bashrc
-fi
+echo "Setting WARPFIELD_CORE path..."
+echo "export WARPFIELD_CORE=$PWD/warpfield_core/" > $HOME/.warpfield_init.sh
+export WARPFIELD_CORE=$PWD/warpfield_core
+
+echo "Setting WARPFIELD_EMP_DEBUG=False..."
+echo "export WARPFIELD_EMP_DEBUG=False" >> $HOME/.warpfield_init.sh
+export WARPFIELD_EMP_DEBUG=False
+
+echo "Setting WARPFIELD_EMP_DEFAULT_TABLE..."
+echo "export WARPFIELD_EMP_DEFAULT_TABLE=${PWD}/warpfield_core/public_data/cloud_data/warpfield_emp_1d_v1.0.ecsv" >> $HOME/.warpfield_init.sh
+export WARPFIELD_EMP_DEFAULT_TABLE=${PWD}"/warpfield_core/public_data/cloud_data/warpfield_emp_1d_v1.0.ecsv"
+
+echo "Adding WARPFIELD to PYTHONPATH"
+echo "export PYTHONPATH=\$PYTHONPATH:$PWD" >> $HOME/.warpfield_init.sh
+export PYTHONPATH=\$PYTHONPATH:$PWD
+
+echo "creating alias"
+echo "alias warpfield_gui=\"python3 $PWD/warpfield_emp_viewer/WARPEMP_VIEWER_v${VERSION}.py\"" >> $HOME/.warpfield_init.sh
+alias warpfield_gui="python3 $PWD/warpfield_emp_viewer/WARPEMP_VIEWER_v${VERSION}.py"
+
+echo -e "\n#The following line was added by the WARPFIELD Installer script" >> $HOME/.bashrc
+echo -e "source ~/.warpfield_init.sh\n" >> $HOME/.bashrc
+
+echo -e "\n\n Installation finished.\nYou can now run the WARPFIELD Viewer and Population synthesis GUI"
+echo "To Run type: warpfield_gui"
